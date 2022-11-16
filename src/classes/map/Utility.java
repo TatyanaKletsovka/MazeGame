3adbee88d939e810eb845f7300435c6f64fd3ee9
TEk <t.kambarov@syberry.com>
2022-11-17 03:23:10 +0600

    509927: Add labyrinth class to work with map

---
+++ src/classes/map/Utility.java
@@ -0,0 +1,19 @@
+package classes.map;
+
+import java.util.Arrays;
+
+public class Utility {
+    public static char[][] deepCopy(char[][] original) {
+
+        if (original == null) {
+            return null;
+        }
+
+        final char[][] result = new char[original.length][];
+        for (int i = 0; i < original.length; i++) {
+            result[i] = Arrays.copyOf(original[i], original[i].length);
+        }
+        return result;
+    }
+
+}
